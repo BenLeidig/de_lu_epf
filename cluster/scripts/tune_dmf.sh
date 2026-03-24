ad7bffeb5c592510eaca37ab0cf6001c9971a269
Benjamin Leidig <ben.leidig@gmail.com>
2026-03-24 11:44:08 -0500

    DMF and VTLM SLURM script fixes

--- cluster/scripts/tune_dmf.sh
+++ cluster/scripts/tune_dmf.sh
@@ -5,5 +5,5 @@ model_class=("en" "lgbmr" "rfr" "svr" "xgbr")
 
 # Submit job for each model class
 for mod in "${model_class[@]}"; do
-    sbatch "cluster/slurm/tune_dmf/tune_{mod}.slurm"
+    sbatch "cluster/slurm/tune_dmf/tune_${mod}.slurm"
 done
