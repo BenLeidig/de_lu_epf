Corrected vital bug in VMD IMF residual calculation. Cannot believe this; basic numpy. Also, refactored scripts/features/processing/merge_data.py, with functions being added to src/de_lu_epf/data/processing.py. Maybe one day we can have VTLM...

scripts/data/features/vmd_features.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     # Set paths
     BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-    cfg_path = BASE_DIR / "configs/model/vmd_hyperparams_config.yaml"
+    cfg_path = BASE_DIR / "configs/models/vmd_hyperparams_config.yaml"
     data_path = BASE_DIR / "data/processed"
     model_path = BASE_DIR / "models"
 
@@ -24,8 +24,8 @@ if __name__ == "__main__":
         ["train", "train_val"], ["val", "test"], ["partial", "full"]
     ):
         ## Load data
-        df_train = pd.read_parquet(data_path / f"ann_data/{train}.parquet")
-        df_test = pd.read_parquet(data_path / f"ann_data/{test}.parquet")
+        df_train = pd.read_parquet(data_path / f"ann/{train}.parquet")
+        df_test = pd.read_parquet(data_path / f"ann/{test}.parquet")
 
         ## Get price arrays
         train_price = df_train["price"].to_numpy()
@@ -44,14 +44,14 @@ if __name__ == "__main__":
         df_test[[f"imf{i}" for i in range(1, 6)]] = test_imfs
 
         ## Add residual IMF (raw - sum(IMFs)) to DataFrames
-        df_train["imf_resid"] = train_price - np.sum(train_imfs)  # type: ignore
-        df_test["imf_resid"] = test_price - np.sum(test_imfs)  # type: ignore
+        df_train["imf_resid"] = train_price - np.sum(train_imfs, axis=1)  # type: ignore
+        df_test["imf_resid"] = test_price - np.sum(test_imfs, axis=1)  # type: ignore
 
         ## Save IMF DataFrame
-        df_train.to_parquet(data_path / f"vmd_data/{train}.parquet", index=True)
-        df_test.to_parquet(data_path / f"vmd_data/{test}.parquet", index=True)
+        df_train.to_parquet(data_path / f"hybrid/{train}.parquet", index=True)
+        df_test.to_parquet(data_path / f"hybrid/{test}.parquet", index=True)
 
         ## Save VMD model
-        dump(vmd, BASE_DIR / f"models/vmd_models/{folder}/vmd.pkl")
+        dump(vmd, BASE_DIR / f"models/hybrid/{folder}/vmd.pkl")
 
     print("+" * 8, " `vmd_features.py` completed. ", "+" * 8)

src/de_lu_epf/models/architectures.py
@@ -22,6 +22,18 @@ class DirectMultiStepForecaster(RegressorMixin, BaseEstimator):
         self.model_class = model_class
 
     def fit(self, X, Y):
+        """Fit each hour's expert model. `X` and `Y` should be indexed daily, with each column in `Y` representing an hour of the next day to be forecasted.
+
+        Args:
+            X (array-like): Feature matrix to fit all expert models on. Should be indexed daily.
+            Y (array-like): Target matrix to fit expert models on. Columns should be sorted ascending by hour of next day.
+
+        Raises:
+            ValueError: If the number of hyperparameter sets in `params` specified during initialization is not equal to the number of targets (should be 24).
+
+        Returns:
+            DirectMultiStepForecaster: self.
+        """
         # n0 = X.shape[0] = Y.shape[0]
         # p = X.shape[1]
         # q = Y.shape[1] = len(self.params)
@@ -35,7 +47,9 @@ class DirectMultiStepForecaster(RegressorMixin, BaseEstimator):
         self.models_ = {}
 
         if len(self.params) != Y.shape[1]:
-            raise ValueError("Number of parameter sets must match number of targets")
+            raise ValueError(
+                "Number of hyperparameter sets must match number of targets"
+            )
 
         for hour in range(Y.shape[1]):
             model = self.model_class(**self.params[hour])
@@ -53,6 +67,65 @@ class DirectMultiStepForecaster(RegressorMixin, BaseEstimator):
         X = validate_data(self, X, reset=False)
 
         preds = [self.models_[hour].predict(X) for hour in sorted(self.models_)]
+        preds = np.column_stack(preds)
+
+        if self.target_names_ is not None:
+            return pd.DataFrame(preds, columns=self.target_names_, index=idx)
+
+        return preds
+
+
+class HybridForecaster(RegressorMixin, BaseEstimator):
+    def __init__(self, params: dict, model_class):
+        self.params = params
+        self.model_class = model_class
+
+    def fit(self, X, Y):
+        """Fit each mode's expert model. `X` and `Y` should be indexed hourly, with each column in `Y` representing an IMF (or IMF residual) to be forecasted.
+
+        Args:
+            X (array-like): Feature matrix to fit all expert models on. Should be indexed hourly.
+            Y (array-like): Target matrix to fit expert models on. Columns should be sorted ascending by mode number (with last one represent the IMF residual).
+
+        Raises:
+            ValueError: If the number of hyperparameter sets in `params` specified during initialization is not equal to the number of targets (should be 6).
+
+        Returns:
+            HybridForecaster: self.
+        """
+        # n0 = X.shape[0] = Y.shape[0]
+        # p = X.shape[1]
+        # q = Y.shape[1] = len(self.params)
+        # --------
+        # X.shape = (n0, p)
+        # Y.shape = (n0, q)
+        # y_i.shape = (n0, )
+
+        self.target_names_ = getattr(Y, "columns", None)
+        X, Y = validate_data(self, X, Y, multi_output=True)
+        self.models_ = {}
+
+        if len(self.params) != Y.shape[1]:
+            raise ValueError(
+                "Number of hyperparameter sets must match number of targets"
+            )
+
+        for mode in range(Y.shape[1]):
+            model = self.model_class(**self.params[mode])
+            model.fit(X, Y[:, mode])
+            self.models_[mode] = model
+
+        return self
+
+    def predict(self, X):
+        # n = X.shape[0]
+        # preds.shape = (n, q)
+
+        check_is_fitted(self, "models_")
+        idx = getattr(X, "index", None)
+        X = validate_data(self, X, reset=False)
+
+        preds = [self.models_[mode](X) for mode in sorted(self.models_)]
         preds = np.column_stack(preds)
 
         if self.target_names_ is not None:

src/de_lu_epf/models/predicting.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from joblib import load
 from yaml import safe_load
@@ -43,8 +44,11 @@ def fetch_full_scaler(model_type: str):
     return load(MODEL_DIR / "scaler.pkl")
 
 
-def format_dmf_preds(preds):
-    return preds.to_numpy().flatten()
+def format_preds(preds, which: str):
+    if which == "dmf":
+        return np.asarray(preds).flatten()
+    elif which == "hybrid":
+        return np.asarray(preds).sum(axis=1)
 
 
 def get_predictions_dmf(model_name: str):
@@ -76,10 +80,54 @@ def get_predictions_dmf(model_name: str):
     )
 
     y_train_val_pred = pd.DataFrame(
-        data=format_dmf_preds(Y_train_val_pred), columns=["price"], index=train_val_idx
+        data=format_preds(preds=Y_train_val_pred, which="dmf"),
+        columns=["price"],
+        index=train_val_idx,
     )
     y_test_pred = pd.DataFrame(
-        data=format_dmf_preds(Y_test_pred), columns=["price"], index=test_idx
+        data=format_preds(preds=Y_test_pred, which="dmf"),
+        columns=["price"],
+        index=test_idx,
+    )
+
+    return y_train_val_pred, y_test_pred
+
+
+def get_predictions_hybrid(model_name: str):
+    model_type = "hybrid"
+
+    df_train_val_scaled, _ = fetch_train_val_data(model_type)
+    df_test_scaled, _ = fetch_test_data(model_type)
+
+    features, targets = fetch_features_targets(model_type)
+    X_train_val_scaled = df_train_val_scaled[features]
+    X_test_scaled = df_test_scaled[features]
+
+    hybrid = fetch_fitted(model_type=model_type, model_name=model_name)
+    scaler = fetch_full_scaler(model_type)
+
+    Y_train_val_scaled_pred = hybrid.predict(X_train_val_scaled)
+    Y_test_scaled_pred = hybrid.predict(X_test_scaled)
+
+    train_val_idx = X_train_val_scaled.index
+    test_idx = X_test_scaled.index
+
+    Y_train_val_pred = scaler.inverse_transform(
+        pd.concat([X_train_val_scaled, Y_train_val_scaled_pred], axis=1)
+    )[targets]
+    Y_test_pred = scaler.inverse_transform(
+        pd.concat([X_test_scaled, Y_test_scaled_pred], axis=1)
+    )[targets]
+
+    y_train_val_pred = pd.DataFrame(
+        data=format_preds(preds=Y_train_val_pred, which="hybrid"),
+        columns=["price"],
+        index=train_val_idx,
+    )
+    y_test_pred = pd.DataFrame(
+        data=format_preds(preds=Y_test_pred, which="hybrid"),
+        columns=["price"],
+        index=test_idx,
     )
 
     return y_train_val_pred, y_test_pred