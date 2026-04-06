4a7c242c3f6a0521a737e008a2514083fc486fa1
Benjamin Leidig <ben.leidig@gmail.com>
2026-04-06 15:27:43 -0500

    Minor bug fix in saving the .pt model files for VTLM training.

--- scripts/models/train/hybrid/train_vtlm.py
+++ scripts/models/train/hybrid/train_vtlm.py
@@ -1,14 +1,14 @@
 import os
 from pathlib import Path
 
-from joblib import dump
+import torch
 
 from de_lu_epf.models.architectures import TCN_LSTM_MHA
 from de_lu_epf.models.training import get_best_vtlm_params, get_fitted_ann
 
 if __name__ == "__main__":
     BASE_DIR = Path(__file__).parent.parent.parent.parent.parent
-    MODEL_DIR = BASE_DIR / "models/hybrid/full"
+    MODEL_DIR = BASE_DIR / "models/hybrid/full/vtlm"
     DATA_DIR = BASE_DIR / "data/processed/hybrid"
 
     model_name = "vtlm"
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     batch_size, params = get_best_vtlm_params(target_col=target_col)
 
-    dmf = get_fitted_ann(
+    model = get_fitted_ann(
         target_col=target_col,
         batch_size=batch_size,
         params=params,
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         max_epochs=epochs,
     )
 
-    dump(dmf, MODEL_DIR / f"vtlm/{target_col}_{model_name}.pkl")
+    torch.save(model.state_dict(), MODEL_DIR / f"{target_col}_{model_name}.pt")
