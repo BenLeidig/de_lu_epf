12cc68ceab31658b7eab324599e9ffb968092e43
Benjamin Leidig <ben.leidig@gmail.com>
2026-04-26 20:28:35 -0500

    Removed a temporary local run line from Hybrid final model training scripts.

--- scripts/models/train/hybrid/train_vt.py
+++ scripts/models/train/hybrid/train_vt.py
@@ -8,27 +8,26 @@ if __name__ == "__main__":
     model_class = arc.TCN
     model_type = "hybrid"
 
-    # i = int(os.environ["SLURM_ARRAY_TASK_ID"])
+    i = int(os.environ["SLURM_ARRAY_TASK_ID"])
     targets = ["imf1", "imf2", "imf3", "imf4", "imf5", "imf_resid"]
 
-    for i in range(len(targets)):
-        target_col = targets[i]
+    target_col = targets[i]
 
-        batch_size, params = get_best_ann_params(
-            target_col=target_col, model_name=model_name, model_type=model_type
-        )
+    batch_size, params = get_best_ann_params(
+        target_col=target_col, model_name=model_name, model_type=model_type
+    )
 
-        get_fitted_ann(
-            model_class=model_class,
-            params=params,
-            target_col=target_col,
-            model_type=model_type,
-            model_name=model_name,
-            seq_len=24 * 7 * 2,
-            pred_len=24,
-            stride=24,
-            batch_size=batch_size,
-            patience=5,
-            max_epochs=100,
-            random_state=0,
-        )
+    get_fitted_ann(
+        model_class=model_class,
+        params=params,
+        target_col=target_col,
+        model_type=model_type,
+        model_name=model_name,
+        seq_len=24 * 7 * 2,
+        pred_len=24,
+        stride=24,
+        batch_size=batch_size,
+        patience=5,
+        max_epochs=100,
+        random_state=0,
+    )
